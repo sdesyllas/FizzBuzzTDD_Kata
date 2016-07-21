0533125da49d972c7b052da178160590de89b88a
Spyros Desyllas <sdesyllas@hotmail.com>
2016-07-21 16:13:13 +0100

    folder and solution tide up

---
+++ FizzBuzzTdd.Tests/FizzBuzzTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace FizzBuzzTdd.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(2, ExpectedResult = "2")]
+        [TestCase(3, ExpectedResult = "Fizz")]
+        [TestCase(4, ExpectedResult = "4")]
+        [TestCase(5, ExpectedResult = "Buzz")]
+        [TestCase(15, ExpectedResult = "FizzBuzz")]
+        public string FizzBuzz_Can_Return_Correct_Result(int input)
+        {
+            // Arrange
+            var mockLogService = new Mock<ILogService>();
+            mockLogService.Setup(x => x.Log(It.IsAny<string>())).Callback( () => Console.WriteLine("mock logger called")).Verifiable();
+            var fizzBuzzService = new FizzBuzzService(mockLogService.Object);
+
+            // Act
+            var result = fizzBuzzService.Calculate(input);
+            
+            // Assert
+            mockLogService.Verify(x => x.Log(It.IsAny<string>()), Times.Exactly(2));
+
+            return result;
+        }
+    }
+
+    public class LogService : ILogService
+    {
+        public void Log(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    public interface ILogService
+    {
+        void Log(string s);
+    }
+
+    public class FizzBuzzService
+    {
+        private readonly ILogService _logService;
+
+        public FizzBuzzService(ILogService logService)
+        {
+            _logService = logService;
+        }
+
+        public string Calculate(int input)
+        {
+            _logService.Log($"input : {input}");
+            string result = String.Empty;
+            if (input%3 == 0)
+            {
+                result = "Fizz";
+            }
+            if (input%5 == 0)
+            {
+                result += "Buzz";
+            }
+            if (result == String.Empty)
+            {
+                result = input.ToString();
+            }
+            _logService.Log($"result : {result}");
+            return result;
+        }
+    }
+}
--- FizzBuzzTdd.Tests/Properties/AssemblyInfo.cs
+++ FizzBuzzTdd.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FizzBuzzTdd")]
+[assembly: AssemblyTitle("FizzBuzzTdd.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FizzBuzzTdd")]
+[assembly: AssemblyProduct("FizzBuzzTdd.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8570ff4d-1086-4899-9544-dd4f47b24b95")]
+[assembly: Guid("722b80f3-1473-4bc1-8d6d-cc03e411653b")]
 
 // Version information for an assembly consists of the following four values:
 //
